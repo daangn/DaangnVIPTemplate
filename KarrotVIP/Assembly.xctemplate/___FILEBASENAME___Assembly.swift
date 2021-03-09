205de26d310e63a74297aca4604b3501f79ace25
OhKanghoon <ggaa96@naver.com>
2021-03-09 12:22:32 +0900

    Add Markup

--- KarrotVIP/Assembly.xctemplate/___FILEBASENAME___Assembly.swift
+++ KarrotVIP/Assembly.xctemplate/___FILEBASENAME___Assembly.swift
@@ -3,20 +3,20 @@
 import Swinject
 
 final class ___VARIABLE_assemblyName___Assembly: Assembly {
-
   func assemble(container: Container) {
     let registerFunctions: [(Container) -> Void] = [
-      self.dosomething,
+      self.registerSomething,
     ]
 
     registerFunctions.forEach { $0(container) }
   }
 }
 
-private extension ___VARIABLE_assemblyName___Assembly {
 
-  func dosomething(container: Container) {
+// MARK: - Something
+
+private extension ___VARIABLE_assemblyName___Assembly {
+  func registerSomething(container: Container) {
     
   }
-
 }
